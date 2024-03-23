3b526ab278bd995a308586a418a4fc9a8f3e433d
Stephen Margheim <stephen.margheim@gmail.com>
2024-03-23 11:13:47 +0100

    Use tabs in feather test

--- test/test_feather.rb
+++ test/test_feather.rb
@@ -3,7 +3,7 @@
 require "test_helper"
 
 class TestFeather < Minitest::Test
-  def test_that_it_has_a_version_number
-    refute_nil ::Feather::VERSION
-  end
+	def test_that_it_has_a_version_number
+		refute_nil ::Feather::VERSION
+	end
 end
